ee8e790d3761657f1494b837d0856830e49ad62f
Bouteille158 <72262554+Bouteille158@users.noreply.github.com>
2020-10-03 09:22:38 +0200

    Ajout ip cloud mongoDB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/bezcoder"
+    //url: "mongodb://localhost:27017/bezcoder"
+    url: "mongodb+srv://bouteille:LJk8rHH2pZ46HN15@qwesttv-test.yznnj.mongodb.net/bezcoder?retryWrites=true&w=majority"
   };
