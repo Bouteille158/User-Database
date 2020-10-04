6b8f8fa9613725af2dfe41f337e02f613cbc4893
Bouteille158 <72262554+Bouteille158@users.noreply.github.com>
2020-10-04 19:03:15 +0200

    Updated local db url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //url: "mongodb://localhost:27017/QwestTV-test"
+    //url: "mongodb://localhost:27017/bdd"
     url: "mongodb+srv://bouteille:LJk8rHH2pZ46HN15@qwesttv-test.yznnj.mongodb.net/QwestTV-test?retryWrites=true&w=majority"
   };
